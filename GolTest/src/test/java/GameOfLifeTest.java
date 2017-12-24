3cd88f652e05aa3417b0394e36368da96bdde34c
lvkaitao <598087448@qq.com>
2017-12-24 21:24:53 +0800

    完成：判断一个位置上是否存在一个活的细胞，如果该位置在没有超出边界，并且值为true，则return 1，否则return 0

--- GolTest/src/test/java/GameOfLifeTest.java
+++ GolTest/src/test/java/GameOfLifeTest.java
@@ -1,6 +1,6 @@
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class GameOfLifeTest {
 
@@ -20,15 +20,41 @@ public class GameOfLifeTest {
         LifeTable[5][7]=true;
         GameOfLife Gol=new GameOfLife(LifeTable,TableSize);
         assertEquals(0,Gol.ifLiveCell(-2,-2));
+        assertEquals(1,Gol.ifLiveCell(5,7));
     }
 
     @Test
     public void getNeighborNum() {
-
+        int TableSize=10;
+        boolean[][] LifeTable=new boolean[TableSize][TableSize];
+        for(int i=0;i<TableSize;i++){
+            for(int j=0;j<TableSize;j++){
+                LifeTable[i][j]=false;
+            }
+        }
+        LifeTable[5][4]=true;
+        LifeTable[5][5]=true;
+        LifeTable[5][6]=true;
+        LifeTable[5][7]=true;
+        GameOfLife Gol=new GameOfLife(LifeTable,TableSize);
+        assertEquals(2,Gol.getNeighborNum(5,5));
+        assertEquals(1,Gol.getNeighborNum(5,4));
     }
 
     @Test
     public void setNeighborNum() {
+        int TableSize=10;
+        boolean[][] LifeTable=new boolean[TableSize][TableSize];
+        for(int i=0;i<TableSize;i++){
+            for(int j=0;j<TableSize;j++){
+                LifeTable[i][j]=false;
+            }
+        }
+        LifeTable[5][4]=true;
+        LifeTable[5][5]=true;
+        LifeTable[5][6]=true;
+        LifeTable[5][7]=true;
+        GameOfLife Gol=new GameOfLife(LifeTable,TableSize);
 
     }
 
